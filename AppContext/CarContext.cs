9f09cca2012df4304f3700bb67d60cc99c7b1a07
Aleksei Evstratov <alekseie@dmdevelopment.ru>
2023-08-30 11:08:48 +0700

    Конфигурация контекста данных. Обработка запросов контроллером. Добавление БД в проект

--- AppContext/CarContext.cs
+++ AppContext/CarContext.cs
@@ -1,14 +1,17 @@
 ﻿using CarStore.Models;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarStore.AppContext
 {
     public class CarContext : DbContext
-    {
-        public DbSet<Car> Cars { get; set; }
+    { 
+
+        public DbSet<Car> Cars { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO брать строку подключения из appsettings.json
+            optionsBuilder.UseSqlite("Data Source=CarStoreDB.db");
+        }
     }
 }
--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarStore.AppContext;
+using CarStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,37 +14,52 @@ namespace CarStore.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
+        CarContext db = new CarContext();
+
         // GET: api/Cars
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Car> Get()
         {
-            return new string[] { "Car1", "Car2" };
+            return db.Cars;
         }
 
         // GET api/Cars/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Car Get(int id)
         {
-            return $"Car id = {id}";
+            return db.Cars.Find(id);
         }
 
         // POST api/Cars
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void AddCar([FromBody] Car car)
         {
+            db.Cars.Add(car);
+            db.SaveChanges();
         }
 
         // PUT api/Cars
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void EditCar(int id, [FromBody] Car car)
         {
+            if (id == car.Id)
+            {
+                db.Entry(car).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
         }
 
         // DELETE api/Cars/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void DeleteCar(int id)
         {
-            
+            Car car = db.Cars.Find(id);
+            if (car != null)
+            {
+                db.Cars.Remove(car);
+                db.SaveChanges();
+            }
         }
     }
 }
